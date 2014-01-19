50a82410ebbf7e3a541425a0714d190c6fa924a1
huangy6 <huangy6@yahoo.com>
2014-01-18 22:58:43 -0500

    modified:   app/db/Answers.scala 	modified:   play-polls.h2.db 	modified:   play-polls.lock.db

--- app/db/Answers.scala
+++ app/db/Answers.scala
@@ -5,7 +5,7 @@ import play.api.db.DB
 import play.api.Play.current
 
 // use a case class to model the stuff you save in a table
-case class Answers(id: Long, content: String, votes: BigInt, questionID: BigInt)
+case class Answers(id: Long, content: String, votes: BigInt, questionid: BigInt)
 
 object Answer {
   // creates the table in the database
@@ -15,10 +15,10 @@ object Answer {
     } 
   }
   
-  val insertStmt = "INSERT INTO answers (content, votes) VALUES ({content}, {votes})"
-  def addAnswers(content: String, votes: BigInt, questionID: BigInt) {
+  val insertStmt = "INSERT INTO answers (content, votes, questionid) VALUES ({content}, {votes}, {questionid})"
+  def addAnswers(content: String, votes: BigInt, questionid: BigInt) {
     DB.withConnection { implicit c =>
-      SQL(insertStmt).on('content -> content, 'votes -> votes).execute()  
+      SQL(insertStmt).on('content -> content, 'votes -> votes, 'questionid -> questionid).execute()  
     }
   }
   
@@ -27,26 +27,36 @@ object Answer {
     val query = SQL(idQuery).on('id -> id)
     DB.withConnection { implicit c =>
       query().map {
-        case Row(theId: Long, content: String, votes: BigInt, questionID: BigInt) => Answers(theId, content, votes)
+        case Row(theId: Long, content: String, votes: BigInt, questionid: BigInt) => Answers(theId, content, votes, questionid)
       }.headOption
     }
   }
   
-  val contentQuery = "SELECT id, content, votes FROM Answers WHERE content={content}"
+  val contentQuery = "SELECT id, content, votes, questionid FROM Answers WHERE content={content}"
   def getBycontent(content: String): Option[Answers] = {
     val query = SQL(contentQuery).on('content -> content)
     DB.withConnection { implicit c => 
       query().map {
-        case Row(theId: Long, content: String, votes: BigInt) => Answers(theId, content, votes)
+        case Row(theId: Long, content: String, votes: BigInt, questionid: BigInt) => Answers(theId, content, votes, questionid)
+      }.headOption  
+    }
+  }
+
+  val questionQuery = "SELECT id, content, votes, questionid FROM Answers WHERE questionid={questionid}"
+  def getByquestion(questionid: String): Option[Answers] = {
+    val query = SQL(questionQuery).on('questionid -> questionid)
+    DB.withConnection { implicit c => 
+      query().map {
+        case Row(theId: Long, content: String, votes: BigInt, questionid: BigInt) => Answers(theId, content, votes, questionid)
       }.headOption  
     }
   }
   
   def listAnswers(): List[Answers] = {
-    val query = SQL("SELECT id, content, votes FROM Answers")
+    val query = SQL("SELECT id, content, votes, questionid FROM Answers")
     DB.withConnection { implicit c =>
       query().map {
-        case Row(theId: Long, content: String, votes: BigInt) => Answers(theId, content, votes)
+        case Row(theId: Long, content: String, votes: BigInt, questionid: BigInt) => Answers(theId, content, votes, questionid)
       }.toList
     }
   }
@@ -60,7 +70,8 @@ object Answer {
     |  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     |  content VARCHAR(100),
     |  votes BIGINT
-    |  FOREIGN KEY (votes) REFERENCES polls (id)
+    |  questionid BIGINT
+    |  FOREIGN KEY (questionid) REFERENCES polls (id)
   )""".stripMargin
 
   
